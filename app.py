bc1f3f157258640852677b7b2219e8802d364fbe
Nat <al222310397@gmail.com>
2026-01-28 13:53:55 -0600

    Agregue codigo

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from controllers.HomeController import blueprint_home
+
+def create_app():
+    app = Flask(__name__)
+    app.register_blueprint(blueprint_home,url_prefix='/api/v1')
+    @app.route('/')
+    def home():
+        return {'msj': 'hola mundo jsjsj'}
+
+    return app
+
+
+if __name__ == '__main__':
+    app = create_app()
+    app.run(debug=  True, 
+            host= '0.0.0.0')
